4e08b6fb7bccbd82f939f53364eb242916accdd2
impperson17 <sultanovchyngyz@gmail.com>
2024-09-29 15:59:12 +0000

    added class2

---
+++ Class2/group.tf
@@ -0,0 +1,11 @@
+resource "aws_iam_group" "developers1" {
+  name = "developers1"
+}
+
+resource "aws_iam_group" "developers2" {
+  name = "hello"
+}
+
+resource "aws_iam_group" "developers3" {
+  name = "developers3"
+}
